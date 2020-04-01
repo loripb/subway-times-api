df3b51f50a0791d3d411ec81d02abde71387ead9
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-01 17:18:20 -0400

    fixes bug preventing seeds from being created and deleted

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# data reset
+StopLine.destroy_all
+StarredStop.destroy_all
+Stop.destroy_all
+Line.destroy_all
+User.destroy_all
 
 ########################## parser
 
 file = File.open("./public/stops.txt")
-file_data = file.readlines
+file_data = file.readlines # reads data from stops.txt
 
 $stops = file_data.map do |line|
-  line.split(',,')
+  line.split(',,') # splits string by duble commas
 end
 
 file.close
 
 def parse_stops
   $stops.map do |text_line|
-    Stop.create(stop_id: text_line[0], name: text_line[1])
+    Stop.create(stop_id: text_line[0], name: text_line[1]) # [0] stop id, [1] stop name
   end
 end
 
@@ -28,26 +34,26 @@ parse_stops
 
 Line.create(name: "7", feed_id: 51)
 
-StopLine.create(stop_id: Stop.find_by(stop_id: 726).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 725).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 724).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 723).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 721).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 720).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 719).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 718).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 716).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 714).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 713).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 712).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 711).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 710).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 709).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 708).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 707).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 706).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 705).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 702).id, line_id: Line.first)
-StopLine.create(stop_id: Stop.find_by(stop_id: 701).id, line_id: Line.first)
+StopLine.create(stop_id: Stop.find_by(stop_id: 726).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 725).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 724).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 723).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 721).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 720).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 719).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 718).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 716).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 714).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 713).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 712).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 711).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 710).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 709).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 708).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 707).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 706).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 705).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 702).id, line_id: Line.first.id)
+StopLine.create(stop_id: Stop.find_by(stop_id: 701).id, line_id: Line.first.id)
 
 puts "DONE!"

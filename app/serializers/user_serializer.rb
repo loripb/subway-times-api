217d4bbaed361d3d5fa4edf845e5169eeae42f83
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-02 13:03:08 -0400

    edits user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest
+  attributes :id, :username
+  has_many :starred_stops
+  has_many :stops, through: :starred_stops
 end

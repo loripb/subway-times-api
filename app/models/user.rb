af3f18f9e94f79bcf9bd9b99f3561eef73698de1
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-02 13:10:54 -0400

    adds user auth to backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :starred_stops
   has_many :stops, through: :starred_stops
+
+  validates :username, uniqueness: true
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :stops
   resources :lines
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/login", to: "users#login"
+  get "/persist", to: "users#persist"
 end

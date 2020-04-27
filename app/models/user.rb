4a4890d2078f94e41bce3f51c34f9f254753ca30
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-27 16:12:59 -0400

    adds validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :starred_stops
   has_many :stops, through: :starred_stops
 
-  validates :username, uniqueness: true
+  validates :username, length: { in: 4..16 }, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
 end

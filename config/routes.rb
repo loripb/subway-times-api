37a2eeafe8aee40b9edd6fab8e3bd5861d9f1dc3
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-30 23:20:11 -0400

    cleans up useable routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :stop_lines
-  resources :starred_stops
-  resources :stops, only: [ :show ]
+  # resources :stop_lines
+  resources :starred_stops, only: [ :index, :create, :destroy ]
+  resources :stops, only: [ :index, :show ]
   resources :lines, only: [ :index, :show ]
-  resources :users
+  resources :users, only: [ :index, :create ]
 
   post "/login", to: "users#login"
   get "/persist", to: "users#persist"

0948d288fed9e4ca53cf0bd5cbd55c276ffa66bc
Lori Boyd <lori.p.boyd@gmail.com>
2020-05-28 20:20:15 -0400

    Adds the show route to starred_stops

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # resources :stop_lines
-  resources :starred_stops, only: [ :index, :create, :destroy ]
+  resources :starred_stops, only: [ :index, :create, :destroy, :show ]
   resources :stops, only: [ :index, :show ]
   resources :lines, only: [ :index, :show ]
   resources :users, only: [ :index, :create ]

59d3a0ad82452fd731b29d46aa43309c4959c8ee
Lori Boyd <lori.p.boyd@gmail.com>
2020-06-04 13:00:41 -0400

    adds ability to fetch for arrival times in StopLine

--- app/controllers/stop_lines_controller.rb
+++ app/controllers/stop_lines_controller.rb
@@ -1,2 +1,7 @@
 class StopLinesController < ApplicationController
+  def find
+    @stop_line = StopLine.find_by(line_id: params[:line_id], stop_id: params[:stop_id])
+
+    render json: @stop_line.get_stop_arrival_times
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # resources :stop_lines
+  resources :stop_lines
   resources :starred_stops, only: [ :index, :create, :destroy, :show ]
   resources :stops, only: [ :index, :show ]
   resources :lines, only: [ :index, :show ]
   resources :users, only: [ :index, :create ]
 
+  get "/find", to: "stop_lines#find"
   post "/login", to: "users#login"
   get "/persist", to: "users#persist"
 end
